2844b03bd48ce5a7d76a1ea0c913c69f04789bf0
Sigurd Mazanti <sigmn@live.dk>
2024-06-06 21:25:41 +0200

    app.css

--- views/restaurant.php
+++ views/restaurant.php
@@ -33,6 +33,15 @@ $items = $q->fetchAll();
     <button onclick="add_to_cart('<?php echo $item['item_id']?>')">Add to cart</button>
   </div>
 <?php endforeach ?>
+<section class="comment-section">
+  <h2>Comments <span>(0)</span></h2>
+  <!-- foreach -->
+
+  <form id="insert_restaurant_comment" method="POST" class="">
+    <label for="user_comment">Add comment</label>
+    <textarea name="user_comment color" required placeholder="Text..."></textarea>
+  </form>
+</section>
 
 <?php
 require_once __DIR__.'/_footer.php';
